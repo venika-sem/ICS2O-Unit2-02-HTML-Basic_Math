01b30c5f5f14db3e1f75a0cfb41e0ee077839368
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-11 18:24:38 +0000

    changed index.html

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Venika Sem All rights reserved
+//
+// Created by: Venika Sem
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+    // this function does basic math
+
+  document.getElementById('add-math').innerHTML = '<p>The perimeter is = ' + (5 + 3 + 5 + 3) + ' cm</p>'
+
+  document.getElementById('multiply-math').innerHTML = '<p>The area is = ' + (5 * 3) + ' ㎠</p>'
+}
